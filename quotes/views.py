a6ec9a563107b8e49a1083c61b6df1a12eaf09c0
Sean Cho <seancho@bu.edu>
2025-02-07 21:58:17 -0500

    FINAL PUSH

--- quotes/views.py
+++ quotes/views.py
@@ -5,13 +5,19 @@ import random
 quotes = [
     "We can't solve problems by using the same kind of thinking we used when we created them.",
     "Try not to become a man of success. Rather, become a man of value.",
-    "Anyone who has never made a mistake has never tried anything new."
+    "Anyone who has never made a mistake has never tried anything new.",
+    "It has become appallingly obvious that our technology has exceeded our humanity.",
+    "Imagination is more important than knowledge.",
+    "The true sign of intelligence is not knowledge but imagination."
     ]   
 
 images = [
     'images/einstein1.png',
     'images/einstein2.png',
     'images/einstein3.png',
+    'images/einstein4.png',
+    'images/einstein5.png',
+    'images/einstein6.png'
     ]
 def base(request):
     '''Define a view to show the 'base.html' template.'''
@@ -53,9 +59,15 @@ def show_all(request):
         "quote1": quotes[0],
         "quote2": quotes[1],
         "quote3": quotes[2],
+        "quote4": quotes[3],
+        "quote5": quotes[4],
+        "quote6": quotes[5],
         "image1": images[0],
         "image2": images[1],
         "image3": images[2],
+        "image4": images[3],
+        "image5": images[4],
+        "image6": images[5],
     }
     return render(request, template, context)
 
