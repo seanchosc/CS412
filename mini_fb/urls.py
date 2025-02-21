3147b0d80389291efee3416f24576f768c19725d
Sean Cho <seancho@bu.edu>
2025-02-21 18:09:15 -0500

    updated to support show_all_profiles, base.html started

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,9 +2,9 @@
 # mini_fb/urls.py
 
 from django.urls import path
-from .views import ShowAllView # our view class definition 
+from .views import ShowAllProfilesView # our view class definition 
 
 urlpatterns = [
     # map the URL (empty string) to the view
-    path('', ShowAllView.as_view(), name='show_all_profiles'), # generic class-based view
+    path('', ShowAllProfilesView.as_view(), name='show_all_profiles'), # generic class-based view
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 from .models import Profile # import Profile model
 from django.views.generic import ListView
 
-class ShowAllView(ListView):
-    '''Create a subclass of ListView to display all blog articles.'''
+class ShowAllProfilesView(ListView):
+    '''Create a subclass of ListView to display all profiles.'''
 
     model = Profile # retrieve objects of type Profile from the database
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
+
+    
