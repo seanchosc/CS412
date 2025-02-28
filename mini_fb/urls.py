0ff017fdb1d8ecffc60eb1329cb86426822636c1
Sean Cho <seancho@bu.edu>
2025-02-28 15:50:45 -0500

    create profile functionality implemented

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,12 +2,12 @@
 # mini_fb/urls.py
 
 from django.urls import path
-from .views import ShowAllProfilesView, BaseView, ShowProfilePageView # our view class definition 
+from .views import ShowAllProfilesView, BaseView, ShowProfilePageView, CreateProfileView # our view class definition 
 
 urlpatterns = [
     # map the URL (empty string) to the view
     path('', BaseView.as_view(), name='base'), # base view
     path('show_all_profiles/', ShowAllProfilesView.as_view(), name='show_all_profiles'), # show all profiles view
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'), # show profile page view
-    ### line to test commit 
+    path('profile/create/', CreateProfileView.as_view(), name='create_profile'), # show create profile view
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -3,7 +3,8 @@
 # Define the views for the blog app:
 from django.shortcuts import render
 from .models import Profile # import Profile model
-from django.views.generic import ListView, DetailView #ListView for Base and SAP, Detail for ShowProfile
+from django.views.generic import ListView, DetailView, CreateView # ListView for Base and SAP, Detail for ShowProfile, CreateView for CreateProfileView
+from .forms import CreateProfileForm # Import the create profile form from forms
 
 # BASE VIEW
 class BaseView(ListView):
@@ -26,3 +27,13 @@ class ShowProfilePageView(DetailView):
     model = Profile # retrieve objects of type Profile from the database
     template_name = 'mini_fb/show_profile.html' # show_profile_page template
     context_object_name = 'profile' # how to find the data in the template file
+
+### Create Profile View ###
+class CreateProfileView(CreateView):
+    '''A view to handle creation of a new Profile.
+    (1) display the HTML form to user (GET)
+    (2) process the form submission and store the new Profile object (POST)
+    '''
+    form_class = CreateProfileForm # use the CreateProfileForm class in forms
+    template_name = 'mini_fb/create_profile_form.html' # show create_profile_form template
+    context_object_name = 'form' # how to find the data in the template file
