8cfcdae90a7c3546299cb1f4fce183741d0c8a89
Sean Cho <seancho@bu.edu>
2025-02-28 14:06:06 -0500

    added in line annotations and minor styling fix

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
     
     def get_status_messages(self):
         ''' accessor method to obtain all status messages for this Profile '''
-        messages = StatusMessage.objects.filter(profile = self)
+        messages = StatusMessage.objects.filter(profile = self) # filter StatusMessage objects by their profile (foreign key), and order them by timestamp
         return messages
     
 class StatusMessage(models.Model):
