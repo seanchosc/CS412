a222b7254c39623753fa6c1d920e5cdc09ac6ea1
Sean Cho <seancho@bu.edu>
2025-04-04 17:57:47 -0400

    load_data() complete

--- voter_analytics/models.py
+++ voter_analytics/models.py
@@ -6,24 +6,27 @@ class Voter(models.Model):
     ''' Represents a registered voter  '''
     lastName = models.TextField()           # *    Last Name 
     firstName = models.TextField()          # *    First Name
-    streetNumber = models.IntegerField()    # *    Residential Address - Street Number
+    streetNumber = models.TextField()    # *    Residential Address - Street Number
     streetName = models.TextField()         # *    Residential Address - Street Name
-    aptNumber = models.IntegerField()       # *    Residential Address - Apartment Number
-    zipCode = models.IntegerField()         # *    Residential Address - Zip Code
+    aptNumber = models.TextField()       # *    Residential Address - Apartment Number
+    zipCode = models.TextField()         # *    Residential Address - Zip Code
     dob = models.CharField(max_length=10)   # *    Date of Birth
     dor = models.CharField(max_length=10)   # *    Date of Registration
     affiliation = models.CharField(max_length=2) # Party Affiliation (**note, this is a 2-character wide field**)
-    precinctNumber = models.IntegerField()  # *    Precinct Number
+    precinctNumber = models.TextField()  # *    Precinct Number
     v20 = models.BooleanField()             # *    v20state
     v21s = models.BooleanField()            # *    v21town
     v21p = models.BooleanField()            # *    v21primary
     v22 = models.BooleanField()             # *    v22general
     v23 = models.BooleanField()             # *    v23town
-    voterScore = models.IntegerField()      # *    Voter Score
+    voterScore = models.TextField()      # *    Voter Score
 
     def __str__(self):
         ''' Returns a spring representation of a voter '''
-        return f'{self.firstName} {self.lastName}, DOB: {self.dob}, Party: {self.affiliation}, Zipcode: {self.zipCode}'
+        if self.affiliation[1] == ' ':
+            party = self.affiliation[0]
+
+        return f'{self.firstName} {self.lastName}| DOB: {self.dob}, Party: {party}, Zipcode: {self.zipCode}'
     
     # begin load_data() function to process .csv file
     def load_data():
@@ -32,29 +35,35 @@ class Voter(models.Model):
         filename = 'voter_analytics/data/newton_voters.csv'
         f = open(filename)
         f.readline() # discard headers
-
-        for row in range(5):
-            line = f.readline().strip()
-            fields = line.split(',')
+        for line in f:
+            fields = line.strip().split(',')
             # create a new instance of Voter object with this record from CSV
-            voter = Voter(
-                        lastName=fields[1],
-                        firstName=fields[2],
-                        streetNumber= fields[3],
-                        streetName = fields[4],
-                        aptNumber = fields[5],
-                        zipCode = fields[6],
-                        dob = fields[7],
-                        dor = fields[8],
-                        affiliation = fields[9],
-                        precinctNumber = fields[10],
-                        v20 = fields[11],
-                        v21s = fields[12],
-                        v21p = fields[13],
-                        v22 = fields[14],
-                        v23 = fields[15],
-                        voterScore = fields[16]
-                        )
-            print(f'Created result: {voter}')
+            try:
+                voter = Voter(
+                            lastName=fields[0],
+                            firstName=fields[1],
+                            streetNumber=fields[2] if fields[2] else "N/A",
+                            streetName=fields[3],
+                            aptNumber=fields[4] if fields[4] else "N/A",
+                            zipCode=fields[5] if fields[5] else "N/A",
+                            dob=fields[6],
+                            dor=fields[7],
+                            affiliation=fields[8].strip(),
+                            precinctNumber=fields[9] if fields[9] else "N/A",
+                            v20=fields[10].strip().upper() == "TRUE",
+                            v21s=fields[11].strip().upper() == "TRUE",
+                            v21p=fields[12].strip().upper() == "TRUE",
+                            v22=fields[13].strip().upper() == "TRUE",
+                            v23=fields[14].strip().upper() == "TRUE",
+                            voterScore=fields[15] if fields[15] else "N/A"
+                            )
+                voter.save() # commit to database
+                print(f'Created result: {voter}')
+
+            except:
+                print(f"Skipped: {fields}")
+        print(f'Done. Created {len(Voter.objects.all())} voters.')
+        f.close()
+
         
     
