3cece84d50206ce45465f6e2e813becff2fbd448
Sean Cho <seancho@bu.edu>
2025-04-04 18:54:17 -0400

    Views and urls updated

--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -5,7 +5,13 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from . models import Voter # import the voter mdel
 
-class ResultsListView(ListView):
+# BASE VIEW
+class BaseView(ListView):
+    model = Voter # retrieve objects of type Voter from the database
+    template_name = 'voter_analytics/base.html' # base template
+    context_object_name = 'voter' # how to find the data in the template file
+
+class VoterListView(ListView):
     '''View to display voter information'''
 
     template_name = 'voter_analytics/voter.html'
