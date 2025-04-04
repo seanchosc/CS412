5ac8aef30a097779770a3eac1c55b56ce99907c1
Sean Cho <seancho@bu.edu>
2025-04-04 18:50:06 -0400

    url updated

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.VoterListView.as_view(), name='home'), # display voter view
+    path(r'', views.BaseListView.as_view(), name='home'), # display base view
     path(r'voters', views.VoterListView.as_view(), name='voters_list'), # display voter view
 ]   
